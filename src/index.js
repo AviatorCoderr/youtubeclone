4d63ac3ffe851a3f44746a69a79f26a9092fe4d1
AviatorCoderr <124370316+AviatorCoderr@users.noreply.github.com>
2024-03-11 09:43:08 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import dotenv from "dotenv"
 import mongoose, { mongo } from "mongoose";
 import {DB_NAME} from "./constants.js" ;
 import connectDB from  "./db/index.js"
-
+import { app } from "./app.js";
 dotenv.config({
     path: './env'
 })
 
-connectDB
+connectDB()
 .then(() => {
     app.on("error", (err) => {
         console.log("error in app running ", err)
